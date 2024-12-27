2f0871a237c52e06c70cb4e564c98e6e39fcc985
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-12-27 12:17:10 +0100

    Add networks size to NASBench201

--- _extractors/nasbench201.py
+++ _extractors/nasbench201.py
@@ -11,13 +11,16 @@ def extract_result(results: dict[str, Any]) -> dict[str, float]:
     # valid_epoch_key = f"x-valid@{max_epoch-1}"
     valid_epoch_key = f"x-valid@{max_epoch-1}"
     row = {
-        "train_time": results["train_times"][max_epoch - 1],
+        # https://github.com/D-X-Y/NATS-Bench/blob/main/nats_bench/api_utils.py#L1306
+        "train_time": sum(results["train_times"][e] for e in range(max_epoch)),
         "train_loss": min(results["train_losses"][e] for e in range(max_epoch)),
         "train_acc": max(results["train_acc1es"][e] for e in range(max_epoch)),
-        "val_time": results["eval_times"][valid_epoch_key],
+        # https://github.com/D-X-Y/NATS-Bench/blob/main/nats_bench/api_utils.py#L1332-L1337
+        "val_time": sum(results["eval_times"][f"x-valid@{e}"] for e in range(max_epoch)),
         "val_loss": results["eval_losses"][valid_epoch_key],
         "val_acc": results["eval_acc1es"][valid_epoch_key],
-        "latency": results["latency"][0],
+        "latency": results["latency"][0],  # ms
+        "model_size": results["params"],  # MB
     }
     return {k: float(v) for k, v in row.items()}
 
